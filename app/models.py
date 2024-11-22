3ff38606e0e4809eb2e8720a3dc49214b953bb41
gmerlerati98 <g.merlerati59@gmail.com>
2024-11-22 15:51:33 +0000

    first push

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Float, Date
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Expense(Base):
+
+    __tablename__ = 'expenses'
+
+    id = Column(Integer, primary_key=True, index=True)
+    description = Column(String, index=True)
+    amount = Column(Float)
+    category = Column(String, index=True)
+    date = Column(Date)
+
+
