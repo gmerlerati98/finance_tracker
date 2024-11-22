e16448a1dbf157c856799576b3e7a637f991f6d7
gmerlerati98 <g.merlerati59@gmail.com>
2024-11-22 21:19:40 +0000

    Delete virtual env

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, Integer, String, Float, Date
 from sqlalchemy.ext.declarative import declarative_base
 
+#Section is to build the database models definitions
+
 Base = declarative_base()
 
 class Expense(Base):
@@ -14,3 +16,12 @@ class Expense(Base):
     date = Column(Date)
 
 
+class Income(Base):
+
+    __tablename__ = 'incomes'
+
+    id = Column(Integer, primary_key=True, index=True)
+    description = Column(String, index=True)
+    amount = Column(Float)
+    category = Column(String, index=True)
+    date = Column(Date)
